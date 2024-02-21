95f8bc9577fc534bc5ed922faf29d4c27da069d2
Erik Mosich <erikmosich@gmail.com>
2024-02-21 10:45:50 -0300

    lista de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Products } from "./components/Products.jsx";
+import { products } from "./mocks/products";
 
 function App() {
- 
   return (
     <>
-     <h1>Carrito de compras</h1>
+      <h1>Carrito de compras</h1>
+      <Products products={products} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,25 @@
-import React, { Component } from 'react';
+import './Products.css';
+import {AddToCartIcon} from "./Icons";
 
-class Products extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        );
-    }
-}
+export function Products({ products }) {
+    return (
+      <main className="products">
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              <img src={product.thumbnail} alt={product.title} />
+              <div>
+                <strong> {product.title} </strong> - ${product.price}
+              </div>
+              <div>
+                <button>
+                    <AddToCartIcon/> 
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
+    );
+ }
 
-export default Products;
