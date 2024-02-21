7a3c27dca15c07f2205dcc11e5aff34071a4a0ea
Erik Mosich <erikmosich@gmail.com>
2024-02-21 15:03:38 -0300

    filtros

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import { Products } from "./components/Products.jsx";
-import { products } from "./mocks/products";
+import { products as initialProducts } from "./mocks/products";
 
 function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState ({
+    category: 'laptops',
+    minPrince: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrince &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
     <>
       <h1>Carrito de compras</h1>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </>
   );
 }
